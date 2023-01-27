cb5a891d8f772c4a333c9a9b6c5a291ef7c0f87e
vipindubey098 <vipindubey098@gmail.com>
2023-01-27 15:35:40 +0530

    Added viewsets to profile api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
-
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from profiles_api import views
 
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset') 
+# to register viewsets with our viewsets, we gonna access hello/viewsets, router will create all 4 urls for us, we don't need to specify, second arguments is the viewset we wish to register this url. Finally we need to specify a base name for our viewset. (hello-viewset)->This is gonna be used for retreving the url in our routers. Now add this in urlpatterns.
+
 urlpatterns= [
-    path('hello-view/', views.HelloApiView.as_view()), #as_view() - It is standard function that we call to convert our API view class to be rendered by urls.
+    path('hello-view/', views.HelloApiView.as_view()), 
+    #as_view() - It is standard function that we call to convert our API view class to be rendered by urls.
+    path('', include(router.urls)),
+    # As we register routes with our routers it generates a list of urls that are associated for our viewsets. It figures out the url that are required for all of the functions that we need to use our viewset and that it generates this url list which we can pass in using the path functions and include function. The reason we specify a blank string here is because we don't want to put a prefix to this URL. We just want all of the URL in the blank part and thats how you register view sets.
+
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,6 +5,8 @@ from rest_framework import status
 from profiles_api import serializers
 # We are gonna tell our API view what data to expect when making post, put and patch requests
 
+from rest_framework import viewsets
+
 class HelloApiView(APIView):
     """Test API View"""
     
@@ -49,3 +51,52 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete an objects"""
         return Response({'method': 'DELETE'})
+
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test Api Viewsets"""
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """Return a hello message"""
+
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message':'Hello!','a_viewset': a_viewset})
+
+
+    def create(self, request):
+        """Create a new hello message"""
+        #Retrieving our serializers
+        serializer = self.serializer_class(data=request.data)
+        
+        #validation
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+        return Response({'http_method': 'GET'})
+    
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'http_method': 'PUT'})
+    
+    def partial_update(self, request, pk=None):
+        """Handle updating part of object"""
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handle Removing an object"""
+        return Response({'http_method': 'DELETE'})
